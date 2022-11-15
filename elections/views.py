7c7df26656396b09154e4083277b18c0d565d948
Daniel Peach <djpeach96@gmail.com>
2022-11-15 00:10:50 -0500

    black format

--- elections/views.py
+++ elections/views.py
@@ -27,10 +27,12 @@ def vote(request):
             ctx["error"] = f"The {election.name} is not yet active"
             ctx["error_level"] = "warning"
             return render(request, "elections/vote.html", ctx)
-        raceIds = request.POST["raceIds"].split(',')[:-1]
+        raceIds = request.POST["raceIds"].split(",")[:-1]
         voter = Voter.objects.get(pk=request.POST["voterId"])
         if voter.vote_set.count() > 0:
-            ctx["error"] = f"You have already voted. If you believe this is an error, please contact the teacher"
+            ctx[
+                "error"
+            ] = f"You have already voted. If you believe this is an error, please contact the teacher"
             ctx["error_level"] = "warning"
             return render(request, "elections/vote.html", ctx)
         for raceId in raceIds:
@@ -44,22 +46,21 @@ def vote(request):
             if session_votes.count() > 0:
                 previous_vote = session_votes.first()
                 send_mail(
-                    'Voter Fraud Detected',
+                    "Voter Fraud Detected",
                     f"A session previously used to cast this vote: {request.get_host()}/admin/elections/vote/{previous_vote.id}/change/ was used to try to cast a vote for {candidate.name} in {race.name} by {voter.name}. Most likely either {previous_vote.voter.name} is attempting to vote on others' behalf. Best to delete the previous vote, and inform both voters what occurred.",
-                    'dpeachesdev@gmail.com',
-                    ['peachgarrett05@gmail.com'],
+                    "dpeachesdev@gmail.com",
+                    ["peachgarrett05@gmail.com"],
                     fail_silently=False,
                 )
-                ctx["error"] = f"Voter fraud detected. Admin has been notified. " \
-                               f"If you believe this is an error, please contact the teacher"
+                ctx["error"] = (
+                    f"Voter fraud detected. Admin has been notified. "
+                    f"If you believe this is an error, please contact the teacher"
+                )
                 ctx["error_level"] = "danger"
 
                 return render(request, "elections/vote.html", ctx)
             Vote.objects.create(
-                voter=voter,
-                race=race,
-                candidate=candidate,
-                session_id=session_id
+                voter=voter, race=race, candidate=candidate, session_id=session_id
             )
         return redirect("results")
     return render(request, "elections/vote.html", ctx)
